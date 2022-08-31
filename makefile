b8eca5d62ff1c5c0e9c7f76ef50e85ff437b206b
seattlenomnom <mark.crapser@gmail.com>
2022-08-30 23:10:03 -0700

    initial commit of project

---
+++ makefile
@@ -0,0 +1,4 @@
+# makefile for search4substring
+#
+search4substring: search4substring.c
+	gcc -g search4substring.c -o ./Build/search4substring
---
+++ search4substring.c
@@ -0,0 +1,55 @@
+/* search4substring.c
+ * sourcefile for search4substring, a program to determine if a substring, s2
+ * exists inside a string, s1.
+ *
+ * An exercise from Kochan, Stephen G., "Programmming in C 3rd ed.," Sams
+ * Publishing, page 231, ex. 5.
+ *
+ * Programmer: Mark Crapser.
+ *
+ * Tues Aug 30 2022
+ * Get outline of program added, commited, and pushed.
+ *
+*/
+
+
+#include <stdio.h>
+
+int s2ins1(char s1[], char s2[]);
+
+int main(int argc, char *argv[]) {
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    return(0);
+}
+
+int s2ins1(char s1[], char s2[]) {
+
+    int index;
+    index = -1;
+
+
+
+
+
+
+
+    return(index);
+}
