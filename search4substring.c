bf7d50cdd4189d03a319dcc2680eb265e6e1149f
seattlenomnom <mark.crapser@gmail.com>
2022-09-19 16:15:32 -0700

    changed function name to find_string

--- search4substring.c
+++ search4substring.c
@@ -10,18 +10,39 @@
  * Tues Aug 30 2022
  * Get outline of program added, commited, and pushed.
  *
+ * Sun Sept 4 2022
+ * Program getting more complicated. I feel that I am losing focus. I am
+ * programming from the console.
+ *
 */
 
 
 #include <stdio.h>
 
-int s2ins1(char s1[], char s2[]);
+#define MAX_LENGTH 81
+
+int string_len(char s[]);
+
+void get_string(char s[], int max);
+
+int find_string(char s1[], char s2[]);
 
 int main(int argc, char *argv[]) {
 
+    char s1[81] = {'\0'};
+    char s2[81] = {'\0'};
+
+    int i;          /* index for s1[] */
+    int j;          /* index for s2[] */
+
+    /* get s1 */
+
+    get_string(s1, MAX_LENGTH);
 
 
+    /* get s2 */
 
+    get_string(s2, MAX_LENGTH);
 
 
 
@@ -40,7 +61,7 @@ int main(int argc, char *argv[]) {
     return(0);
 }
 
-int s2ins1(char s1[], char s2[]) {
+int find_string(char s1[], char s2[]) {
 
     int index;
     index = -1;
@@ -52,4 +73,42 @@ int s2ins1(char s1[], char s2[]) {
 
 
     return(index);
+}
+
+int string_len(char s[]) {
+
+    int length;
+    int index;
+
+    length = 0;
+    index = 0;
+
+    while(s[index] != '\0')
+        ++index;
+
+    length = index;
+
+    return(length);
+}
+
+
+void get_string(char s[], int max) {
+
+    char c;
+    int i;
+
+    c = getchar();
+    while(c != EOF) {
+        s[i] = c;
+        c = getchar();
+    }
+
+
+
+
+
+
+
+
+
 }
